b636e755641adb3a054162cb3618eeba233ab4ff
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-12-05 11:31:45 +0000

    V.0.0.2 Created a method to add text to the field name

--- selenium/selenium_walkthrough/selenium_walkthrough.rb
+++ selenium/selenium_walkthrough/selenium_walkthrough.rb
@@ -3,8 +3,18 @@ require 'selenium-webdriver'
 class SeleniumQaToolsForm
 
   def initialize
-    @chrome_driver = Selenium::WebDriver.form :chrome
+    @chrome_driver = Selenium::WebDriver.for :chrome
     @practise_form_url = 'http://toolsqa.com/automation-practice-form/'
-    @first_name_field = 'firstname'
+    @first_name_field_name = 'firstname'
+  end
+
+  def visit_practise_form
+    @chrome_driver.get(@practise_form_url)
+  end
+
+  def input_firstname_field(text)
+    @chrome_driver.find_element(:name, @first_name_field_name).send_keys(text)
   end
 end
+
+test = SeleniumQaToolsForm.new.visit_practise_form
